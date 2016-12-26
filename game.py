e1edea951ef7141b5b63817726c06cc04a247434
Loubna Yousfi <loubnaysf.ly@gmail.com>
2016-12-26 20:12:45 +0100

    Update game.py

--- game.py
+++ game.py
@@ -9,62 +9,79 @@ joueur1 = raw_input("Player1 please enter your name: ")
 joueur2 = raw_input("Player2 please enter your name: ")
 
 print "  "
-print "Choose one of those options : Rock / Paper / Scissors "
+print "Choose one of those options :"
+print " 1 for Rock"
+print " 2 for Paper"
+print " 3 for Scissors"
 print "  "
 
 player1wins = " Congratulations "+joueur1+", you win !"
 player2wins = " Congratulations "+joueur2+", you win !"
 egalite = " It's a tie !"
 waitforit = "Wait For It..."
+partie = 0
+score1 = 0
+score2 = 0
 
-while True:
-    player1 = raw_input(" "+joueur1+" your turn: ")
-    player2 = raw_input(" "+joueur2+" your turn: ")
-
-    if player1 == "Rock":
-        if player2 == "Rock":
+while partie < 6 :
+    player1 = int(raw_input(" "+joueur1+" your turn: "))
+    player2 = int(raw_input(" "+joueur2+" your turn: "))
+    partie += 1
+    if player1 == 1 :
+        if player2 == 1:
             print waitforit
-            time.sleep(1)
+            time.sleep(0.5)
             print egalite
-        elif player2 == "Paper":
+        elif player2 == 2:
             print waitforit
-            time.sleep(1)
+            time.sleep(0.5)
+            print player2wins
+            score2 += 1
+        elif player2 == 3:
+            print waitforit
+            time.sleep(0.5)
             print player1wins
-        elif player2 == "Scissors":
+            score1 += 1
+    elif player2 == 1:
+        if player1 == 2 :
             print waitforit
-            time.sleep(1)
+            time.sleep(0.5)
             print player1wins
-    elif player2 == "Rock":
-        if player1 == "Rock":
+            score1 += 1
+        elif player1 == 3 :
             print waitforit
-            time.sleep(1)
+            time.sleep(0.5)
+            print player2wins
+            score2 += 1
+    elif player1 == 2 :
+        if player2 == 2:
+            print waitforit
+            time.sleep(0.5)
             print egalite
-        elif player1 == "Paper":
+        elif player2 == 3:
             print waitforit
-            time.sleep(1)
+            time.sleep(0.5)
             print player2wins
-        elif player1 == "Scissors":
+            score2 += 1
+    elif player2 == 2:
+        if player1 == 3 :
             print waitforit
-            time.sleep(1)
-            print player2wins
-    elif player1 == "Paper":
-        if player2 == "Paper":
-            print waitforit
-            time.sleep(1)
-            print egalite
-        elif player2 == "Scissors":
-            print waitforit
-            time.sleep(1)
-            print player2wins
-    elif player2 == "Paper":
-        if player1 == "Scissors":
-            print waitforit
-            time.sleep(1)
+            time.sleep(0.5)
             print player1wins
-    elif player1 == "Scissors":
-        if player2 == "Scissors":
+            score1 += 1
+    elif player1 == 3 :
+        if player2 == 3:
             print waitforit
-            time.sleep(1)
+            time.sleep(0.5)
             print egalite
     else :
-        print " Invalid input! You have not entered Rock, Paper or Scissors, try again."
+         print "Invalid Input. Please try again"
+print " "
+print " Game Over !"
+print " "
+if score1 > score2 :
+    print " "+joueur1+", you win this round with "+str(score1)+" points. Congrats !"
+elif score1 < score2 :
+    print " "+joueur2+", you win this round with "+str(score2)+" points. Congrats !"
+else :
+    print " it's a tie ! "
